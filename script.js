ea7f36925a07ceb5dd63212eacf74936125bb7b0
Dhiraj kumar <149323248+Dhirajsingh12@users.noreply.github.com>
2025-01-15 20:03:48 +0530

    init commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const person = {
+    frist_name:'dhiraj',
+    last_name:'kumar',
+    age:22,
+
+}
+console.log(person)
+let num=20
+let num2=30
+console.log(num+num2)
+
+function mul(a,b){
+    console.log(a*b)
+}
+mul(90000,68)
+
+const studet=["dhiraj","pritam "]
+console.log(studet);
+
+alert("dhiraj");
+const value=prompt("what is your name");
+const name = prompt("what is your name");
+document.write("Hello" + name);
