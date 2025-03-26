aa22d15542be26307a3c47b8b0ce4fe10eb1cb66
Dhiraj kumar <149323248+Dhirajsingh12@users.noreply.github.com>
2025-03-26 23:13:19 +0530

    web

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const http = require('http');
+const http = require("http");
+const fs = require("fs");
+const myserver =http.createServer((req,res) =>{
+    const log = '${Date.now()}: ${req.url} new req reciveed\n'
+    fs.appendFile("log.txt",log,(err,data)=>{
+        switch (req.url){
+            case "/":
+            res.end("dk singh1.jpg");
+            break;
+            case "/contact":
+                res.end("hi i am dhiraj kumar");
+                break;
+                default:
+                    res.end("404 not found");
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end("hello welcome to my server");
-});
 
-server.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
+
+        }
+    })
+})
+myserver.listen(2000, () => console.log('server start'));
