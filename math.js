06a991a4687c334c18c0a1e2e37496819e088e43
Dhiraj kumar <149323248+Dhirajsingh12@users.noreply.github.com>
2025-03-26 23:13:10 +0530

    web

---
+++ math.js
@@ -0,0 +1,12 @@
+// function add(a, b){
+//     return a + b;
+// }
+// function sub(a, b){
+//     return a - b;
+// }
+// Module.export = {
+//     addfn: add,
+//     subfn: sub
+// };
+const math = require("fs");
+console.log("math",math);
