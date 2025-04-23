4b1fd7da30a4c123082435d375f55e0191716409
Dhiraj kumar <149323248+Dhirajsingh12@users.noreply.github.com>
2025-04-23 19:48:13 +0530

    js

--- hello.js
+++ hello.js
@@ -1,3 +1,57 @@
-const math =require("./math");
-console.log("math value is", math.addfn(2, 3));
-console.log("math value ",math.subfn(4, 2));
+// let firstName="dhiraj"
+// let lastName="kumar"
+// console.log(firstName);
+// console.log(lastName);
+// var age=50;
+// var age=60;
+// console.log(a);
+// let income =340000;
+//  income =250000;
+// console.log(typeof income);
+// const age=22;
+// console.log(age);
+// let student={
+//     name:"dhiraj",
+//     age:23,
+//     cgpa:8.6,
+//     ispass:true
+// }
+// console.log(student.name);
+// console.log(student.age);
+// console.log(student.cgpa);
+// console.log(student.ispass);
+// let pen={
+//     name:"Parker jotter Standard Ct Ball Pen",
+//     color:"black",
+//     rating:4,
+//     deal_of_the_day:true,
+//     percent_offer:5,
+//     price:270
+// }
+// console.log(pen.name); 
+// console.log(pen.color);
+// console.log(pen.rating);
+// console.log(pen.deal_of_the_day);
+// console.log(pen.percent_offer);
+// console.log(pen.price);
+
+
+// let a=9;
+// let b=9;
+// let sum=a+b;
+// console.log(sum);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a%b);
+// console.log(a**b); 
+// console.log(++a);
+// console.log(b++);
+let a="9";
+let b=5;
+// a+=b;
+// b-=a;
+console.log(a===b);
+console.log(a==b);
+console.log(a!=b);
+console.log(a<=b);
+console.log(a>=b);
