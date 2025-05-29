edd2754a21e3a83d9f7744d31749bd00e2c038ad
Dhiraj kumar <149323248+Dhirajsingh12@users.noreply.github.com>
2025-05-29 23:09:29 +0530

    awate

--- hello.js
+++ hello.js
@@ -186,8 +186,23 @@
 // let b=prompt("enter the second number");
 // let sum=parseInt(a)+parseInt(b)
 // console.log(sum);
-let obj={
-    a:1,
-    b:"dhiraj"
+// let obj={
+//     a:1,
+//     b:"dhiraj"
+// }
+// console.log(obj.a)
+
+async function sleep() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(45);
+        }, 1000);
+    });
 }
-console.log(obj.a)
+
+(async function main() {
+    let a = await sleep();
+    console.log(a);
+    let b = await sleep();
+    console.log(b);
+})();
