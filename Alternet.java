f8bd04c41c616f544e12ad34a426fd12d6f03b56
Dhiraj kumar <149323248+Dhirajsingh12@users.noreply.github.com>
2025-06-03 23:07:49 +0530

    java

---
+++ Alternet.java
@@ -0,0 +1,11 @@
+
+public class Alternet {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        for (int i = 1; i < arr.length; i += 2) {
+            System.out.println(arr[i]);
+
+        }
+    }
+}
